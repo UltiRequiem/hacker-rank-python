27532d7e1d4e748095e042cd10f8d73dfab44c29
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-23 14:17:27 -0500

    :art: Update

--- python/02_if_else.py
+++ python/02_if_else.py
@@ -16,10 +16,10 @@ def weird_or_not(n: int) -> str:
     return "Not Weird" if n in range(2, 5) or n > 20 else "Weird"
 
 
-# What I don't like about this is having to define a dict...
 def weird_or_not_two(n: int) -> str:
-    options = {True: "Not Weird", False: "Weird"}
-    return options[n % 2 == 0 and n in range(2, 6) or n > 20]
+    return {True: "Not Weird", False: "Weird"}[
+        n % 2 == 0 and n in range(2, 6) or n > 20
+    ]
 
 
 if __name__ == "__main__":
