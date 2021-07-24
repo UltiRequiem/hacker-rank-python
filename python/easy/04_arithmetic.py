daa4314eb84ba93d7429f35519c20d341b79d86e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-24 17:26:52 -0500

    :art: Update Exercises

--- python/easy/04_arithmetic.py
+++ python/easy/04_arithmetic.py
@@ -1,5 +1,6 @@
 """
-The provided code stub reads two integers from STDIN,"a" and "b". Add code to print three lines where:
+The provided code stub reads two integers from STDIN,"a" and "b".
+Add code to print three lines where:
 
 The first line contains the sum of the two numbers.
 The second line contains the difference of the two numbers (first - second).
