e8a110c024b04385d795e87dc366c600624aa7e1
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-24 12:12:27 -0500

    :memo: Add Links

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ![Cover](./assets/cover.png)
 
 My HackerRank profile: [@Ultirequiem](https://www.hackerrank.com/Ultirequiem)
+
+## Verified Skills
+
+- [Python (Basic)](https://www.hackerrank.com/certificates/74cb299c09d9)
+
+## Badges
+
+- Python ⭐ (1)
--- python/tests/01_fiz_buzz.py
+++ python/tests/01_fiz_buzz.py
@@ -3,7 +3,7 @@ The Classic FizzBuzz :D
 """
 
 
-def fizzBuzz(num: int):
+def fizz_buzz(num: int):
     if num % 3 == 0 and num % 5 == 0:
         return "FizzBuzz"
     elif num % 3 == 0 and not num % 5 == 0:
@@ -16,5 +16,5 @@ def fizzBuzz(num: int):
 
 if __name__ == "__main__":
     n = 5
-    for i in range(1, n + 1):
-        print(fizzBuzz(i))
+    for num in range(1, n + 1):
+        print(fizz_buzz(num))
