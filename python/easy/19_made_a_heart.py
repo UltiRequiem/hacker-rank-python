2d0e699ddecd5214fed82d7e5ee9bbb3f6baaf50
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-25 10:46:55 -0500

    :art: Update

--- python/easy/19_made_a_heart.py
+++ python/easy/19_made_a_heart.py
@@ -1,23 +1,23 @@
 import math
 
-c = "*"
-width = 40
 
-print((c * 2).center(width // 2) * 2)
+def main(c: str, w: int) -> None:
+    print((c * 2).center(w // 2) * 2)
 
-for i in range(1, width // 10 + 1):
-    print(
-        (
-            (c * int(math.sin(math.radians(i * width // 2)) * width // 4)).rjust(
-                width // 4
+    for i in range(1, w // 10 + 1):
+        print(
+            (
+                (c * int(math.sin(math.radians(i * w // 2)) * w // 4)).rjust(w // 4)
+                + (c * int(math.sin(math.radians(i * w // 2)) * w // 4)).ljust(w // 4)
             )
-            + (c * int(math.sin(math.radians(i * width // 2)) * width // 4)).ljust(
-                width // 4
-            )
+            * 2
         )
-        * 2
-    )
 
-for i in range(width // 4, 0, -1):
-    print((c * i * 4).center(width))
-print((c * 2).center(width))
+    for i in range(w // 4, 0, -1):
+        print(((c * i * 4).center(w)))
+
+    print((c * 2).center(w))
+
+
+if __name__ == "__main__":
+    main("*", 40)
