20c0b53861007d0bdefe3f1aa84fc4f3b710bca2
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-25 21:50:51 -0500

    :art: Better Code

--- python/easy/11_lists.py
+++ python/easy/11_lists.py
@@ -9,30 +9,30 @@ def main() -> None:
     lst = []
     len_of_list = int(input("Enter the len of the list: "))
 
+    commands = {
+        "append": lambda arg: lst.append(arg),
+        "print": lambda: print(lst),
+        "remove": lambda arg: lst.remove(arg),
+        "insert": lambda pos, item: lst.insert(pos, item),
+        "pop": lambda: lst.pop(),
+        "sort": lambda: lst.sort(),
+        "reverse": lambda: lst.reverse(),
+    }
+
     while len(lst) <= len_of_list:
         print(">> ", end="")
         args = input().strip().split(" ")
-
-        if args[0] == "append":
-            lst.append(int(args[1]))
-        elif args[0] == "insert":
-            lst.insert(int(args[1]), int(args[2]))
-        elif args[0] == "remove":
-            lst.remove(int(args[1]))
-        elif args[0] == "pop":
-            lst.pop()
-        elif args[0] == "sort":
-            lst.sort()
-        elif args[0] == "reverse":
-            lst.reverse()
-        elif args[0] == "print":
-            print(lst)
-        elif args[0] == "exit":
-            print("Bye!")
-            exit()
-        else:
+        try:
+            commands[f"{args[0]}"](int(args[1]))
+        except IndexError:
+            commands[f"{args[0]}"]()
+        except TypeError:
+            commands[f"{args[0]}"](int(args[1]), int(args[2]))
+        except KeyError:
             print("That's not a valid command!")
 
 
 if __name__ == "__main__":
     main()
+
+# codereview.stackexchange.com/questions/264384/python-code-style-if-elif-else
